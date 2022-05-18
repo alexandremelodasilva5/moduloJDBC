76dde91e659d95a54e4b25465fb5ad032b29c961
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-18 16:30:59 -0300

    daofactory e daojdbc

--- demo-dao-project/src/application/Program.java
+++ demo-dao-project/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
