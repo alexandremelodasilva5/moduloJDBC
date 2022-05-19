65b33428de02ec6b85f51758606f43ccf86d6915
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-19 10:44:37 -0300

    melhorando método de instanciação

--- demo-dao-project/src/application/Program.java
+++ demo-dao-project/src/application/Program.java
@@ -9,8 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("---Teste 1 = findSellerById---");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 
 	}
