217eb95d846df9a0dfc803c6614f23cee5caedbe
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-19 11:47:49 -0300

    procurando por departamento

--- demo-dao-project/src/application/Program.java
+++ demo-dao-project/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,7 +15,13 @@ public class Program {
 		System.out.println("---Teste 1 = findSellerById---");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-
+		
+		System.out.println("---Teste 2 = findByDepartment---");
+		Department dep= new Department(2, null); 
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller sel:list) {
+			System.out.println(sel);
+		}
 	}
 
 }
