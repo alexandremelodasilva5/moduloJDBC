ec2dfa3eb711b4afe042b5465da9903e7a146976
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-19 11:58:13 -0300

    procurando por todos os vendedores

--- demo-dao-project/src/application/Program.java
+++ demo-dao-project/src/application/Program.java
@@ -12,13 +12,19 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("---Teste 1 = findSellerById---");
+		System.out.println("---Teste 1 = seller findSellerById---");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("---Teste 2 = findByDepartment---");
+		System.out.println("\n---Teste 2 =  seller findByDepartment---");
 		Department dep= new Department(2, null); 
 		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller sel:list) {
+			System.out.println(sel);
+		}
+		
+		System.out.println("\n---Teste 3 = seller findAll---"); 
+		list = sellerDao.findAll();
 		for(Seller sel:list) {
 			System.out.println(sel);
 		}
